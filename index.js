3b005049219a99fa1ffb7b6801c32015aca9469c
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 01:16:54 +0530

    deployment crisis

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// import { connectDB } from "./db/db.connect.js";
-// import dotenv from 'dotenv'
-// import { app } from "./app.js";
+import { connectDB } from "./src/db/db.connect.js";
+import dotenv from 'dotenv'
+import { app } from "./src/app.js";
 
 
-// dotenv.config({
-//     path: '\.env'
-// })
-
-// connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
-
-import express from 'express'
-// import dotenv from 'dotenv'
-// dotenv.config({
-//     path: '.env'
-// })
-const app = express();
-
-app.get('/' , (req,res)=>[
-    res.send("hello from the bottom of my heart")
-])
-
-app.listen(8000 , ()=>{
-    console.log('server running')
+dotenv.config({
+    path: '\.env'
 })
 
-export default app;
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+// import express from 'express'
+// // import dotenv from 'dotenv'
+// // dotenv.config({
+// //     path: '.env'
+// // })
+// const app = express();
+
+// app.get('/' , (req,res)=>[
+//     res.send("hello from the bottom of my heart")
+// ])
+
+// app.listen(8000 , ()=>{
+//     console.log('server running')
+// })
+
+// export default app;
 
