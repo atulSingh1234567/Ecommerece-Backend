a316beb3f0054f01ba076e68e7ae71c750ff8411
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 00:21:04 +0530

    testing with new index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import { connectDB } from "./db/db.connect.js";
+// import { connectDB } from "./db/db.connect.js";
+// import dotenv from 'dotenv'
+// import { app } from "./app.js";
+
+
+// dotenv.config({
+//     path: '\.env'
+// })
+
+// connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+import express from 'express'
 import dotenv from 'dotenv'
-import { app } from "./app.js";
-
-
 dotenv.config({
     path: '\.env'
 })
+const app = express();
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+app.get('/' , (req,res)=>[
+    res.send("hello from the bottom of my heart")
+])
 
+app.listen(process.env.PORT , ()=>{
+    console.log('server running')
+})
 
