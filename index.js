faf0fe0ef96a55d2820c3f280d7c2d8084d438c9
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 03:05:53 +0530

    deployment crisis

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import cors from "cors"
 import cookieParser from 'cookie-parser'
 
 dotenv.config({
-    path: '\.env'
+    path: '.env'
 })
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://ecommerece-frontend.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
