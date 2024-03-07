8ea5be77c608dd3725fa71fe69f91ea9119a6d70
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 13:51:16 +0530

    cors setup debugging

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config({
 const app = express();
 
 app.use(cors({
-    origin: 'https://ecommerece-frontend.vercel.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }))
