197bdc072d2346e1d68af67878e87e7ef6b0476f
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 15:23:26 +0530

    cors setup debugging

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use((req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://ecommerece-frontend.vercel.app/*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+    // next();
   });
 
 import userRouter from './src/routes/user.route.js';
