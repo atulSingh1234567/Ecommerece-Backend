b50523064addb92f0212da1455b43f7c54c0a52c
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 15:26:41 +0530

    cors setup debugging

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
 
-app.use((req, res) => {
+app.use((req, res , next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    // next();
+    next();
   });
 
 import userRouter from './src/routes/user.route.js';
