5485152176dfb9ca5540f694fc3e3ed37ad028de
Atul Singh <sahajkrish7@gmail.com>
2024-03-07 12:45:05 +0530

    cors setup

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ecommerece-frontend.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
